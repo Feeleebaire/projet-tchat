b44a8f7616b56bbf355754cceee82fc08c8d010a
Christophe <Christophe@RAMPAGEIII>
2012-12-24 10:20:13 +0100

    add id on input

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <form name="form" method="POST" action="connect.php">
         <h1>Connexion</h1>
         Nom d&apos;utilisateur : <br />
-        <input type="text" value="" name="login"/><br />
+        <input type="text" value="" id="user" name="login"/><br />
         Mots de passe : <br />
-        <input type="password" value="" name="passwd" /><br />
+        <input type="password" value="" id="passwd" name="passwd" /><br />
         <input type="submit" value="connection" />
         </form>
         pas encore inscrit? : <a href="register.php"> s&apos;enregistrer</a>
