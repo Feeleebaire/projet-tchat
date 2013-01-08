b6489a3e5b1694c0555b732c668fb67088e79e1d
christophe <christophe@archnovo>
2013-01-08 12:39:15 +0100

    add of the verification of the user's folder

--- profil.php
+++ profil.php
@@ -1,17 +1,32 @@
-    <?php
+<?php
     session_start();
     if(isset($_SESSION['user'])){
         include 'config.php';
         $user = $_SESSION['user'];
         $sql = "SELECT user, email FROM users WHERE user ='$user'" ;
-        
-        ?>
+        //repertoire ou l'image serra enregistrer
+        $dossier = "images/";
+        //ici je fais comprandre que c'est un repertoire pour la boucle while
+        $d = dir($dossier);
+        //je regarde dans le dossier images
+        while($entry = $d->read()) {
+            //si il existe un dossier ayant le meme nom que le pseudo
+            if($entry == $user){
+                //je retourne la valeur yes pour dire que le dossier existe
+                $exist_dossier = "yes";
+            }
+            else{
+                $exist_dossier = "no";
+            }
+        }
+        //je referme le dossier
+        $d->close();
+?>
 
 <html>
     <head>
-        <title>Votres profil <?php print $_SESSION['user'];?></title>
+        <title>Votres profil <?php print $user;?></title>
         <link rel="stylesheet" media="screen" href="css/style.css" type="text/css" />
-        
     </head>
     <body>
         Pseudo actuelle: <?php echo $user?><br />
@@ -22,9 +37,19 @@
         entrer un nouvelle e-mail:<br />
         <input type="text" value="" name="email" id="email" /><label id="imge"></label><label id="info2"></label>
         <div id="resultatemail"></div>
-        
+        Vos images:<br />
         <div id="image">
-            
+            <?php 
+            if($exist_dossier == "yes"){
+                echo "coucou<br />\n";
+                //while ($entry = $d->read()){  
+                  //  print '<img src="'.$d.'/'.$user.'/*">';
+                //}
+            }
+            else{
+                echo "Vous n'avez pas encore d'images <br />\n";
+            }
+            ?>
         </div>
         mot de passe:<br />
         <input type="password" value="" name="passwd" id="passwd" /><label id="imgp"></label><label id="mdpinfo"></label><br />
