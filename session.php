d4013f821697f9e764acff8c734c1e24fc484ac4
christophe <christophe@archnovo>
2013-01-10 20:41:18 +0100

    update for picture profile

--- session.php
+++ session.php
@@ -1,6 +1,7 @@
     <?php
     session_start();
     if(isset($_SESSION['user'])){
+        
     ?>
 <html>
     <head>
@@ -10,8 +11,13 @@
     <body>
 
     <?php
+        print '<div id="suser">';
+        print '<img src="'.$_SESSION['pictures'].'" />';
         print $_SESSION['user'];
+        print '</div>';
+        print '<div id="exit">';
         print '<a href="exit.php">deconnexion</a>';
+        print '</div>';
     ?>
 
     <?php }
