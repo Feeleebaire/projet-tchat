fe1831a65e9b2e66b532c330155186a8f2edbae8
christophe <christophe@archnovo>
2013-01-10 13:40:45 +0100

    profil.js change for the control of user information

--- js/profil.js
+++ js/profil.js
@@ -4,33 +4,11 @@ function testForm(form){
     email = form.email.value;
     resuluser = document.getElementById('resultat').textContent;
     resulemail = document.getElementById('resultatemail').textContent;
-    
-    
-    if(!user){
-        alert ("Veuillez remplire le champs nom.");
-        var csu = document.getElementById('user');
-        csu.style.borderColor = 'red';
-        csu.style.backgroundColor = 'lightpink';
-        var csui = document.getElementById('imge');
-        csui.style.backgroundImage = "url('css/images/croix.png')";
-        return false;
-    }
-    else if(!passwd){
-        alert ("Veuillez remplire le champs mot de passe.");
-        var csp = document.getElementById('passwd');
-        csp.style.borderColor = 'red';
-        csp.style.backgroundColor = 'lightpink';
-        var cspi = document.getElementById('imge');
-        cspi.style.backgroundImage = "url('css/images/croix.png')";
-        return false;
-    }
-    else if(!isEmail(email) && email){
+    if(!isEmail(email) && email){
         alert ("adresse email incorrecte");
-
-        
         return false;
     }
-    else if(user == resuluser){
+    else if(user == resuluser && user != ""){
         var cse = document.getElementById('email');
         cse.style.borderColor = 'red';
         cse.style.backgroundColor = 'lightpink';
@@ -39,15 +17,10 @@ function testForm(form){
         alert("ce pseudo est d\351j\340 utiliser");
         return false;
     }
-    else if(email == resulemail){
+    else if(email == resulemail && email !=""){
         alert("cette e-mail est d\351j\340 utiliser");
         return false;
     }
-   else if(user == resuluser && email == resulemail){
-        alert("le pseudo et l'email existe d\351j\340");
-        return false;
-    }
-
 }
 
 function isEmail(email){
